544598d2a1a0a3e6578040d372ca47bce3e269b3
Владислав <voloshenko_vladislav@mail.ru>
2021-12-12 17:10:59 +0700

    upgrade work of the function

--- getTextFromHtml.py
+++ getTextFromHtml.py
@@ -1,31 +1,31 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import typing
+from typing import List
 
 def getTextFromHtml(
-    url: str,
+    urls: List[str],
     filename: str,
-    orientationColumn: bool = True 
+    divider: str = ';'
 ):
     '''
     Getting text from page by url
     ---
     ---
     Arguments:
-    * ulr: str - url of the page from which the data will be received
+    * ulrs: List[str] - url of the page from which the data will be received
     * filename: str - name of file to which data will be written
-    * orientationColumn: bool = True - if True, the data will be written in a column, otherwise in a row
+    * divider: str = ';' - divider of .csv file (';' by default)
     '''
+
     file = open('{}.csv'.format(filename), 'w', encoding="utf-8")
-    html = urlopen(url).read()
-    soup = BeautifulSoup(html, features="html.parser")
-    text = soup.body.get_text()
-    
-    lines = (line.strip() for line in text.splitlines())
-    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-    divider = '\n' if orientationColumn else ';'
-    for l in chunks:
-        if l: file.write(l + divider)
+    for url in urls:
+        html = urlopen(url).read()
+        soup = BeautifulSoup(html, features="html.parser")
+        try:
+            text = soup.body.get_text(divider, strip=True)
+        except AttributeError:
+            continue
+        file.write(url + ':' + divider + text + '\n')
 
     file.close()
     
